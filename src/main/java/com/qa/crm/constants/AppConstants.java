5b4aadde5d093db4f9acc86d55608004898c3c74
Madhusudan J <madhusudanjayasimha1990@gmail.com>
2025-05-04 20:29:40 +0530

    Updated AppConstants.java with new assertion value for homePageHeadersTest

--- src/main/java/com/qa/crm/constants/AppConstants.java
+++ src/main/java/com/qa/crm/constants/AppConstants.java
@@ -11,11 +11,11 @@ public class AppConstants
 	public static final String LOGIN_PAGE_TITLE="Cogmento CRM";
 	public static final String HOME_PAGE_TITLE="Cogmento CRM";
 	
-	public static final int HOME_PAGE_HEADERS_COUNT = 3;
+	public static final int HOME_PAGE_HEADERS_COUNT = 4;
 	
 	public static final String LOGIN_PAGE_FRACTION_URL="cogmento";
 	
-	public static final List<String> EXPECTED_HOME_HEADERS_LIST = List.of("Contacts activity stream","Deals","Today");
+	public static final List<String> EXPECTED_HOME_HEADERS_LIST = List.of("Release Notes","Contacts activity stream","Deals","Today");
 	
 	public static final String FORM_PAGE_HEADER_SUFFIX=" [Active]";
 	public static final long DEFAULT_THREAD_SLEEP_TIME = 5000;
